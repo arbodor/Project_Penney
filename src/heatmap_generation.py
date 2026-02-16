af2980aae340a0a4154fb6d6ba1429307fd66f01
arbodor <lexybodor@icloud.com>
2026-02-16 13:33:35 -0500

    Update heatmap_generation.py

--- src/heatmap_generation.py
+++ src/heatmap_generation.py
@@ -3,16 +3,25 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+def load_data(file_path:str) -> pd.DataFrame:
+    '''
+    Loads a dataset of card sequences from a specified file path.
+    Args:
+        file_path (str): The path to the file containing the dataset.
+    Returns:
+        pd.DataFrame: A pandas data frame containing the average scores.
+    '''
+    df = np.load(file_path)
+    return df
+
 # Create heatmap; percentage values could be useful here, for the sake of clarity
-def create_heatmap(data:pd.DataFrame, save_path:str) -> str:
+def create_heatmap(data:pd.DataFrame) -> str:
     '''
     Creates a heatmap out of the data frame of integers holding the win rates (percentages) after scoring all 
     provided samples.
 
     Arguments:
         data (pd.DataFrame): A data frame of integers containing the win rates.
-        save_path (str): A string of the file path where the heatmap produced will be saved 
-        --- should go in the figures folder ---
     
     Returns:
         String: A statement confirming that the heatmap was saved to the filepath provided in the required 
@@ -21,12 +30,18 @@ def create_heatmap(data:pd.DataFrame, save_path:str) -> str:
         Heatmap will show up as a PNG called "HN_Heatmap_Original.png" (for the original version of the HN game) in 
         the folder identified in the required save_path argument.
     '''
-    heatmap = sns.heatmap(data, annot=True, fmt=".2f", cmap='crest')
-    plt.title("Percentage of Games Won (by Choice)")
+    # need to add custom annotations
+    heatmap = sns.heatmap(data, annot=True, fmt=".2f", cmap='crest', vmin = __, vmax = __)
     plt.xlabel("My choice")
     plt.ylabel("Opponent's choice")
 
-    ### should the filepath just be the figures folder with the name 'HN_Heatmap_Original.png'?
+    #if hn_version = og:
+        #plt.title("Percentage of Games Won (by Choice), Original Version")
+        #save_path = 'figures/HN_Heatmap_Original.png'
+    #elif hn_version = ron:
+        #plt.title("Percentage of Games Won (by Choice), Ron's Version")
+        #save_path = 'figures/HN_Heatmap_Ron's.png'
+
     heatmap.savefig(save_path, dpi=300, bbox_inches='tight') 
 
     return(f'Heatmap saved to {save_path}.')
