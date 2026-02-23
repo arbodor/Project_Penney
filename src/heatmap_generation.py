a460f0d322a8d742568c3a35e0a7adef638b8847
arbodor <lexybodor@icloud.com>
2026-02-23 14:05:19 -0500

    created two custom annotation functions, one for each heatmap (og vs ron)

--- src/heatmap_generation.py
+++ src/heatmap_generation.py
@@ -39,8 +39,43 @@ def load_arrays(file_path:str) -> np.array:
 
     return(f'Arrays loaded from {hnWin_path},{hnTie_path},{ronWin_path},{ronTie_path}.')
 
-# Create heatmap; percentage values could be useful here, for the sake of clarity
-def hn_heatmap(hn_wins:np.array, hn_ties:np.array) -> str:
+def hn_annotations(hn_wins:np.array, hn_ties:np.array, N_BITS=3) -> np.array:
+    '''
+    Creates an array of custom annotations (in the form of 'wins(ties)'for the original version heatmap.
+    Args:
+        hn_wins (np.array): A numpy array of the win rates for the original game version.
+        hn_ties (np.array): A numpy array of the tie rates for the original game version.
+        N_BITS (int): number of bits?
+    Returns:
+        hn_annot (np.array): An array of the custom annotations to go into the hn_heatmap function.
+    '''
+    N_BITS=3
+    hn_annot = np.full(shape=hn_wins.shape, fill_value='', dtype='<U6')
+    for i in range(2**N_BITS):
+        for j in range(2**N_BITS):
+            hn_annot[i,j] = f'{round(hn_wins[i,j]*100)}({round(hn_ties[i,j]*100)})'
+
+    return hn_annot
+
+def ron_annotations(ron_wins:np.array, ron_ties:np.array, N_BITS=3) -> np.array:
+    '''
+        Creates an array of custom annotations (in the form of 'wins(ties)'for the ron version heatmap.
+    Args:
+        hn_wins (np.array): A numpy array of the win rates for the ron game version.
+        hn_ties (np.array): A numpy array of the tie rates for the ron game version.
+        N_BITS (int): number of bits?
+    Returns:
+        ron_annot (np.array): An array of the custom annotations to go into the ron_heatmap function.
+    '''
+    N_BITS=3
+    ron_annot = np.full(shape=ron_wins.shape, fill_value='', dtype='<U6')
+    for i in range(2**N_BITS):
+        for j in range(2**N_BITS):
+            ron_annot[i,j] = f'{round(ron_wins[i,j]*100)}({round(ron_ties[i,j]*100)})'
+
+    return ron_annot
+
+def hn_heatmap(hn_wins:np.array, hn_ties:np.array, hn_annot:np.array) -> str:
     '''
     Creates a heatmap out of the array of integers holding the original game version win rates (percentages) after scoring all 
     provided samples.
@@ -57,7 +92,7 @@ def hn_heatmap(hn_wins:np.array, hn_ties:np.array) -> str:
     axis_labels = ['BBB','BBR','BRB','BRR','RBB','RBR','RRB','RRR']
     df = pd.DataFrame(hn_wins, columns=axis_labels, index=axis_labels)
 
-    heatmap = sns.heatmap(df, annot=True, fmt=".0f", cmap='Blues', vmin=0, vmax=100, cbar=False, square=False)
+    heatmap = sns.heatmap(df, annot=hn_annot, fmt='', cmap='Blues', vmin=0, vmax=100, cbar=False, square=False)
     plt.xlabel("My choice")
     plt.ylabel("Opponent's choice")
     plt.title("My Chance of Win(Draw) by Cards")
@@ -65,7 +100,7 @@ def hn_heatmap(hn_wins:np.array, hn_ties:np.array) -> str:
 
     return(f'Original version heatmap saved to {hn_save_path}.')
 
-def ron_heatmap(ron_wins:np.array, ron_ties:np.array) -> str:
+def ron_heatmap(ron_wins:np.array, ron_ties:np.array, ron_annot:np.array) -> str:
     '''
     Creates a heatmap out of the array of integers holding the ron's game version win rates (percentages) after scoring all 
     provided samples.
@@ -82,7 +117,7 @@ def ron_heatmap(ron_wins:np.array, ron_ties:np.array) -> str:
     axis_labels = ['BBB','BBR','BRB','BRR','RBB','RBR','RRB','RRR']
     df = pd.DataFrame(ron_wins, columns=axis_labels, index=axis_labels)
 
-    heatmap = sns.heatmap(df, annot=True, fmt=".0f", cmap='Blues', vmin=0, vmax=100, cbar=False, square=False)
+    heatmap = sns.heatmap(df, annot=ron_annot, fmt='', cmap='Blues', vmin=0, vmax=100, cbar=False, square=False)
     plt.xlabel("My choice")
     plt.ylabel("Opponent's choice")
     plt.title("My Chance of Win(Draw) by Tricks, N = {total_decks}")
