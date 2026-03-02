91032fc178ec2e556a9bbeaa0f9c9b6074929e22
arbodor <lexybodor@icloud.com>
2026-03-02 13:18:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def main():
     generate_data(num_samples=int(num_samples), random_state=42, save_name='card_sequences2.npy')
     input("Will now score the results of the second batch of games. Press Enter to continue...")
     score_and_save(deck_path='card_sequences2.npy')
-    hn_wins, hn_ties, ron_wins, ron_ties=load_arrays()
+    hn_wins, hn_ties, ron_wins, ron_ties, total_decks=load_arrays()
     hn_heatmap(hn_wins=hn_wins, hn_ties=hn_ties,total_decks=total_decks)
     ron_heatmap(ron_wins=ron_wins,ron_ties=ron_ties,total_decks=total_decks)
 
