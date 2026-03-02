c0e6c122b8f86fb1e6c93ce68e13a433664144a5
arbodor <lexybodor@icloud.com>
2026-03-02 13:16:43 -0500

    revising comments, denominators in total_deck computations

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from src.data_generation import generate_data
 from src.process_data import score_and_save
-from src.heatmap_generation import load_arrays, hn_heatmap
+from src.heatmap_generation import load_arrays, hn_heatmap, ron_heatmap
 def main():
     print("Hello from project-penney!")
     num_samples=input("Enter the number of samples to generate: ")
@@ -13,8 +13,9 @@ def main():
     generate_data(num_samples=int(num_samples), random_state=42, save_name='card_sequences2.npy')
     input("Will now score the results of the second batch of games. Press Enter to continue...")
     score_and_save(deck_path='card_sequences2.npy')
-    hn_wins, hn_ties, ron_wins, ron_ties=load_arrays('data/hn_wins.npy')
-    hn_heatmap(hn_wins, hn_ties)
+    hn_wins, hn_ties, ron_wins, ron_ties=load_arrays()
+    hn_heatmap(hn_wins=hn_wins, hn_ties=hn_ties,total_decks=total_decks)
+    ron_heatmap(ron_wins=ron_wins,ron_ties=ron_ties,total_decks=total_decks)
 
 
 if __name__ == "__main__":
